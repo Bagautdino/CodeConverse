8a4ae265e832f8a6f957259247aa8a1ead99f590
DevSecOps <devsecops@MacBook-Air-DevSecOps.local>
2024-09-12 12:58:19 +0300

    fix: [dev] report

--- report.py
+++ report.py
@@ -13,8 +13,10 @@ class HTMLReport:
         self.report_content = ""
 
     def add_file_summary(self, file_path, summary):
-        self.report_content += markdown2.markdown(self.template.format(file_path=file_path, summary=summary))
-        self.report_content += f"<tr><td>{file_path}</td><td>{summary.replace('\n', '<br>')}</td></tr>"
+        formatted_summary = markdown2.markdown(summary)
+
+        self.report_content += self.template.format(file_path=file_path, summary=formatted_summary)
+        self.report_content += f"<tr><td>{file_path}</td><td>{formatted_summary.replace('\n', '<br>')}</td></tr>"
 
     def finish_report(self):
         final_report = self.template.replace('{{ rows }}', self.report_content)
