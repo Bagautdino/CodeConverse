1cb5e9db83c4198b65e20dffd748758bddddf388
bagautdinov <bagautdinov@biocad.ru>
2024-09-15 22:17:30 +0300

    add: [dev] report php

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -147,7 +147,7 @@ class CodeAnalyzer:
         """
         for root, _, files in os.walk(self.directory):
             for filename in files:
-                if filename.endswith((".py", ".js", ".java", ".cpp", ".c", ".cs", ".ts")):
+                if filename.endswith((".py", ".js", ".java", ".cpp", ".c", ".cs", ".ts", ".php")):
                     file_path = os.path.join(root, filename)
                     current_token = self.get_next_token()
                     logging.info(f"Analyzing file: {file_path}")
